548bd98feb00a48ecd5f8858eaf395eebb8bdf40
Naznin Kamal <najninkamalrimi@gmail.com>
2022-11-11 01:08:29 +0600

    test-code-2

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { MongoClient } = require('mongodb');
 const port = process.env.PORT|| 5000;
 
 // doctor-portals-adminsdk.json
-const serviceAccount = require('./doctor-portals-adminsdk.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
